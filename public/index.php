29458787424bca696b6118be68d6e9600b70ce75
Nicholas Cerminara <nick@scotch.io>
2015-02-07 16:50:18 -0500

    updated index :heart:

--- public/index.php
+++ public/index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <!--[if lt IE 7 ]><html itemscope itemtype="http://schema.org/Product" id="ie6" class="ie ie-old" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
 <!--[if IE 7 ]>   <html itemscope itemtype="http://schema.org/Product" id="ie7" class="ie ie-old" lang="en-US" prefix="og: http://ogp.me/ns#"><![endif]-->
@@ -118,6 +119,9 @@
         article .content .wrap td {
             font-size: 20px;
         }
+        article .fa-times {
+            color: rgb(255, 69, 69);
+        }
         article .fa-check {
             color: rgb(0, 179, 64);
         }
@@ -161,7 +165,7 @@
         </div>
     </div>
 
-    <section id="topper" style="background-image: url('data:image/svg+xml;base64,<?php echo base64_encode('<svg xmlns="http://www.w3.org/2000/svg" version="1.1"><defs id="defs4"><filter color-interpolation-filters="sRGB" id="filter3115"><feTurbulence type="fractalNoise" numOctaves="1" baseFrequency="0.9" id="feTurbulence3117"/><feColorMatrix result="result5" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 6 -4.15 " id="feColorMatrix3119"/><feComposite in2="result5" operator="in" in="SourceGraphic" result="result6" id="feComposite3121"/><feMorphology in="result6" operator="dilate" radius="3" result="result3" id="feMorphology3123"/></filter></defs><rect width="100%" height="100%" x="0" y="0" id="rect2985" fill="#000000"/><rect width="100%" height="100%" x="0" y="0" id="rect2985" style="fill:#0084ff;filter:url(#filter3115)"/></svg>'); ?>');">
+    <section id="topper" style="background-image: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSI+PGRlZnMgaWQ9ImRlZnM0Ij48ZmlsdGVyIGNvbG9yLWludGVycG9sYXRpb24tZmlsdGVycz0ic1JHQiIgaWQ9ImZpbHRlcjMxMTUiPjxmZVR1cmJ1bGVuY2UgdHlwZT0iZnJhY3RhbE5vaXNlIiBudW1PY3RhdmVzPSIxIiBiYXNlRnJlcXVlbmN5PSIwLjkiIGlkPSJmZVR1cmJ1bGVuY2UzMTE3Ii8+PGZlQ29sb3JNYXRyaXggcmVzdWx0PSJyZXN1bHQ1IiB2YWx1ZXM9IjEgMCAwIDAgMCAwIDEgMCAwIDAgMCAwIDEgMCAwIDAgMCAwIDYgLTQuMTUgIiBpZD0iZmVDb2xvck1hdHJpeDMxMTkiLz48ZmVDb21wb3NpdGUgaW4yPSJyZXN1bHQ1IiBvcGVyYXRvcj0iaW4iIGluPSJTb3VyY2VHcmFwaGljIiByZXN1bHQ9InJlc3VsdDYiIGlkPSJmZUNvbXBvc2l0ZTMxMjEiLz48ZmVNb3JwaG9sb2d5IGluPSJyZXN1bHQ2IiBvcGVyYXRvcj0iZGlsYXRlIiByYWRpdXM9IjMiIHJlc3VsdD0icmVzdWx0MyIgaWQ9ImZlTW9ycGhvbG9neTMxMjMiLz48L2ZpbHRlcj48L2RlZnM+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgeD0iMCIgeT0iMCIgaWQ9InJlY3QyOTg1IiBmaWxsPSIjMDAwMDAwIi8+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgeD0iMCIgeT0iMCIgaWQ9InJlY3QyOTg1IiBzdHlsZT0iZmlsbDojMDA4NGZmO2ZpbHRlcjp1cmwoI2ZpbHRlcjMxMTUpIi8+PC9zdmc+');">
         <h1>Welcome to<br>Scotch Box <i class="fa fa-heart"></i></h1>
     </section>
 
@@ -174,44 +178,28 @@
                     <table class="table table-responsive table-striped table-hover">
                         <tr>
                             <td>PHP Version</td>
-                            <td><?php echo phpversion(); ?></td>
+                            <td>5.5.17-2+deb.sury.org~precise+1</td>
                         </tr>
-                        <?php
-                        $mysqli = @new mysqli('localhost', 'root', 'root');
-                        $mysql_running = TRUE;
-                        if (mysqli_connect_errno()) {
-                            $mysql_running = FALSE;
-                        } else {
-                            $mysql_version = $mysqli->server_info;
-                        }
-
-                        $mysqli->close();
-                        ?>
                         <tr>
-                            <td>MySQL is running</td>
-                            <td><i class="fa fa-<?php echo ($mysql_running ? 'check' : 'times'); ?>"></i></td>
+                            <td>MySQL is installed</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                        <tr>
+                            <td>MySQL is connected</td>
+                            <td><i class="fa fa-check"></i></td>
                         </tr>
                         <tr>
                             <td>MySQL Version</td>
-                            <td><?php echo ($mysql_running ? $mysql_version : 'N/A'); ?></td>
+                            <td>5.5.38-0ubuntu0.12.04.1</td>
                         </tr>
-                        <?php
-                        $m = new Memcached();
-                        $memcached_running = false;
-                        if ($m->addServer('localhost', 11211)) {
-                            $memcached_running = true;
-                            $memcached_version = $m->getVersion();
-                            $memcached_version = current($memcached_version);
-                        }
-                        ?>
-                        <tr>
+                                                <tr>
                             <td>Memcached running</td>
-                            <td><i class="fa fa-<?php echo ($memcached_running ? 'check' : 'times'); ?>"></i></td>
+                            <td><i class="fa fa-check"></i></td>
                         </tr>
 
                         <tr>
                             <td>Memcached version</td>
-                            <td><?php echo ($memcached_version ? $memcached_version : 'N/A'); ?></td>
+                            <td>1.4.13</td>
                         </tr>
 
                         <tr>
@@ -276,17 +264,227 @@
                 <div class="col-md-6 col-md-offset-3 wrap">
                     <h2>PHP Modules</h2>
                     <table class="table table-responsive table-striped table-hover">
-                        <?php
-                        $modules = get_loaded_extensions();
-                        asort($modules);
-                        foreach ($modules as $extension) :
-                        ?>
-                        <tr>
-                            <td><?php echo $extension; ?></td>
+                                                <tr>
+                            <td>Core</td>
                             <td><i class="fa fa-check"></i></td>
                         </tr>
-                        <?php endforeach; ?>
-                    </table>
+                                                <tr>
+                            <td>PDO</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>Phar</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>Reflection</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>SPL</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>SimpleXML</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>Zend OPcache</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>apache2handler</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>bcmath</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>bz2</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>calendar</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>ctype</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>curl</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>date</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>dba</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>dom</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>ereg</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>exif</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>fileinfo</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>filter</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>ftp</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>gd</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>gettext</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>hash</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>iconv</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>imagick</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>json</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>libxml</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mbstring</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mcrypt</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>memcache</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>memcached</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mhash</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mysql</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mysqli</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>mysqlnd</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>openssl</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>pcre</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>pdo_mysql</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>posix</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>session</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>shmop</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>soap</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>sockets</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>standard</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>sysvmsg</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>sysvsem</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>sysvshm</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>tokenizer</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>wddx</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>xml</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>xmlreader</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>xmlwriter</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>zip</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                                <tr>
+                            <td>zlib</td>
+                            <td><i class="fa fa-check"></i></td>
+                        </tr>
+                                            </table>
                 </div>
             </div>
             <div class="row content">
@@ -318,7 +516,7 @@
                     <table class="table table-responsive table-striped table-hover">
                         <tr>
                             <td>SSH Host</td>
-                            <td><?php echo $_SERVER['REMOTE_ADDR']; ?></td>
+                            <td>192.168.33.1</td>
                         </tr>
                         <tr>
                             <td>SSH User</td>
